d56c5d1b00476e77e25e97be71dc2614890f8cdc
veronicaxcode <veronica.kaptur@gmail.com>
2022-12-07 10:26:21 +0200

    fix: change edit page URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,14 +38,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/Veronicaxcode/my-docu-website',
+            'https://github.com/Veronicaxcode/my-docu-website/tree/main',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/Veronicaxcode/my-docu-website',
+            'https://github.com/Veronicaxcode/my-docu-website/docs',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
